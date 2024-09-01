dfa386a1dc0ebe082a3203c06b7225dd8f12019d
itaylayzer <itaylayzer@gmail.com>
2024-09-01 07:09:54 +0000

    kart io update

--- src/main.tsx
+++ src/main.tsx
@@ -4,49 +4,36 @@ import "./style/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { P404 } from "./app/P404.tsx";
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
-const CustomRoute = ({ children }: { children: JSX.Element }) => {
+const IgnorePaths = ({ children }: { children: JSX.Element }) => {
   const location = useLocation();
   const pathname = location.pathname;
 
-  // List of file extensions that indicate assets
-  const assetExtensions = [
-    ".fbx",
-    ".png",
-    ".jpg",
-    ".jpeg",
-    ".gif",
-    ".js",
-    ".css",
-    ".glb",
-  ];
+  // Paths to ignore
+  const ignoredPaths = ["/kart.io"];
 
-  // Check if the path ends with one of these extensions
-  const isAsset = assetExtensions.some((ext) => pathname.endsWith(ext));
+  useEffect(() => {
+    const shouldIgnore = ignoredPaths.some((ignoredPath) =>
+      pathname.startsWith(ignoredPath)
+    );
 
-  if (isAsset) {
-    // If it's an asset, don't render the route
-    window.location.href = pathname;
-    return null;
-  }
+    if (shouldIgnore) {
+      // Redirect the browser to let the static server handle the request
+      window.location.href = pathname;
+    }
+  }, [pathname]);
 
-  // Otherwise, render the provided element
   return children;
 };
 
 createRoot(document.body).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-
-      <Route
-        path="*"
-        element={
-          <CustomRoute>
-            <P404 />
-          </CustomRoute>
-        }
-      />
-    </Routes>
+    <IgnorePaths>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<P404 />} />
+      </Routes>
+    </IgnorePaths>
   </BrowserRouter>
 );
